525e3e8e010bb75b26057e5910496fee724c3978
Balach Zehri <balachzehr@hotmail.com>
2024-03-21 02:05:14 +0500

    Add project files.

---
+++ flutterapi/Models/apidbcontext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace flutterapi.Models
+{
+    public class apidbcontext : DbContext
+    {
+        // Constructor to pass options to DbContext base class
+        public apidbcontext(DbContextOptions<apidbcontext> options) : base(options)
+        {
+        }
+
+        // DbSet property for User entity
+        public DbSet<user> Users { get; set; }
+    }
+}
---
+++ flutterapi/Models/user.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace flutterapi.Models
+{
+    public class user
+    {
+        [Key]
+        public int userId { get; set; }
+
+        [Required]
+        public string name { get; set; } = "";
+
+        [Required]
+        public string address { get; set; } = "";
+
+        
+
+
+    }
+}
